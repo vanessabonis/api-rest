8f44b600db3dfb0d23f28b93c0cbde44cca55120
vanessabonis <vanessa1302@gmail.com>
2021-12-15 03:26:13 -0300

    h2 data base, test profile, jpa

--- src/main/java/apirest/completo/entities/User.java
+++ src/main/java/apirest/completo/entities/User.java
@@ -3,6 +3,13 @@ package apirest.completo.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User implements Serializable {
 
 	/**
@@ -11,6 +18,9 @@ public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	//chave primaria
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
